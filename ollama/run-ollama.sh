f70060f566ade49aaf2490ed61e97153d4973701
Dilip Renkila <dilip.renkila@protonmail.com>
2025-07-15 13:27:31 +0200

    Update run-ollama.sh

--- ollama/run-ollama.sh
+++ ollama/run-ollama.sh
@@ -4,4 +4,5 @@ ollama serve &
 ollama list
 ollama pull nomic-embed-text
 ollama pull bge-m3
-#ollama pull deepseek-coder-v2
+ollama pull deepseek-coder-v2
+ollama pull manutic/nomic-embed-code:latest
